7edd008be010e8a5051863567b85710d2a8534b5
munechika-koyo <munechika.koyo@gmail.com>
2024-12-06 10:40:49 +0100

    Fix build.bat to set INSTALL_DIR correctly and replace move commands with xcopy for better file handling

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,22 +1,22 @@
 @echo off
 
-set "INSTALL_DIR"=%PREFIX%\Library\SaxonHE"
-mkdir %INSTALL_DIR%
+set "INSTALL_DIR"=%LIBRARY_PREFIX%\lib\SaxonHE"
 
 :: Install built files
-move saxon-he-*.jar %INSTALL_DIR%
+mkdir %INSTALL_DIR%
+xcopy saxon-he-*.jar %INSTALL_DIR% /s /i /y
 if errorlevel 1 exit 1
 
 mkdir %INSTALL_DIR%\lib
-move lib\* %INSTALL_DIR%\lib
+xcopy lib\* %INSTALL_DIR%\lib /s /i /y
 if errorlevel 1 exit 1
 
 mkdir %INSTALL_DIR%\doc
-move doc\* %INSTALL_DIR%\doc
+xcopy doc\* %INSTALL_DIR%\doc /s /i /y
 if errorlevel 1 exit 1
 
 mkdir %INSTALL_DIR%\notices
-move notices\* %INSTALL_DIR%\notices
+xcopy notices\* %INSTALL_DIR%\notices /s /i /y
 if errorlevel 1 exit 1
 
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
