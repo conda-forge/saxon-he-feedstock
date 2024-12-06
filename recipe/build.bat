34cfa1bdb4dde0264a022efb91291c100d3715ed
munechika-koyo <munechika.koyo@gmail.com>
2024-12-06 10:47:01 +0100

    Fix build.bat to use XCOPY for file copying and ensure INSTALL_DIR is set correctly

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,22 +1,22 @@
-@echo off
+setlocal EnableDelayedExpansion
 
 set "INSTALL_DIR"=%LIBRARY_PREFIX%\lib\SaxonHE"
 
 :: Install built files
 mkdir %INSTALL_DIR%
-xcopy saxon-he-*.jar %INSTALL_DIR% /s /i /y
+XCOPY saxon-he-*.jar %INSTALL_DIR% /s /i /y
 if errorlevel 1 exit 1
 
 mkdir %INSTALL_DIR%\lib
-xcopy lib\* %INSTALL_DIR%\lib /s /i /y
+XCOPY lib\* %INSTALL_DIR%\lib /s /i /y
 if errorlevel 1 exit 1
 
 mkdir %INSTALL_DIR%\doc
-xcopy doc\* %INSTALL_DIR%\doc /s /i /y
+XCOPY doc\* %INSTALL_DIR%\doc /s /i /y
 if errorlevel 1 exit 1
 
 mkdir %INSTALL_DIR%\notices
-xcopy notices\* %INSTALL_DIR%\notices /s /i /y
+XCOPY notices\* %INSTALL_DIR%\notices /s /i /y
 if errorlevel 1 exit 1
 
 :: Copy the [de]activate scripts to %PREFIX%\etc\conda\[de]activate.d.
